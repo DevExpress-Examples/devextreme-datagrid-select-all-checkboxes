ee0b4e32d78bc0db5f144be8a6fd8ee49eb924e3
Artem Kurchenko <artem.kurchenko@devexpress.com>
2025-10-05 21:30:04 +0400

    implement Vue

--- Vue/src/components/HomeContent.vue
+++ Vue/src/components/HomeContent.vue
@@ -1,28 +1,119 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue';
+import { ref, onMounted } from 'vue';
 
 import 'devextreme/dist/css/dx.material.blue.light.compact.css';
-import DxButton from 'devextreme-vue/button';
+import {
+  DxDataGrid,
+  DxColumn,
+  DxGrouping,
+  DxPaging,
+  DxGroupPanel,
+  DxSelection,
+  DxLookup,
+} from 'devextreme-vue/data-grid';
 
-const props = defineProps({
-  text: {
-    type: String,
-    default: 'count',
+import * as AspNetData from 'devextreme-aspnet-data-nojquery';
+
+import 'devextreme/dist/css/dx.material.blue.light.compact.css';
+import GroupSelectionHelper from './GroupRowSelection/GroupRowSelectionHelper';
+import GroupRowComponent from './GroupRowSelection/GroupRowComponent.vue';
+import type { IGroupRowReadyParameter } from '@/types';
+
+const url = 'https://js.devexpress.com/Demos/Mvc/api/DataGridWebApi';
+const dataSource = AspNetData.createStore({
+  key: 'OrderID',
+  loadUrl: `${url}/Orders`,
+  onBeforeSend(_method, ajaxOptions) {
+    ajaxOptions.xhrFields = { withCredentials: true };
   },
 });
-const count = ref(0);
-const buttonText = computed<string>(
-  () => `Click ${props.text}: ${count.value}`
-);
-function clickHandler() {
-  count.value += 1;
+const customersData = AspNetData.createStore({
+  key: 'Value',
+  loadUrl: `${url}/CustomersLookup`,
+  onBeforeSend(_method, ajaxOptions) {
+    ajaxOptions.xhrFields = { withCredentials: true };
+  },
+});
+const shippersData = AspNetData.createStore({
+  key: 'Value',
+  loadUrl: `${url}/ShippersLookup`,
+  onBeforeSend(_method, ajaxOptions) {
+    ajaxOptions.xhrFields = { withCredentials: true };
+  },
+});
+
+const dataGridRef = ref();
+let helper: GroupSelectionHelper;
+
+onMounted(() => {
+  helper = new GroupSelectionHelper(dataGridRef.value!.instance);
+});
+
+function initGroupRow(arg: IGroupRowReadyParameter): Promise<any> {
+  return helper.groupRowInit(arg);
 }
 </script>
 <template>
   <div>
-    <DxButton
-      :text="buttonText"
-      @click="clickHandler"
-    />
+    <DxDataGrid
+      id="gridContainer"
+      ref="dataGridRef"
+      :data-source="dataSource"
+      :remote-operations="true"
+      width="100%"
+      height="604"
+      :show-borders="true"
+    >
+      <template #groupCellTemplate="{ data }">
+        <GroupRowComponent
+          :group-cell-data="data"
+          :init-group-row="initGroupRow"
+        />
+      </template>
+      <DxSelection
+        :deferred="true"
+        mode="multiple"
+        :allow-select-all="true"
+        show-check-boxes-mode="always"
+      />
+      <DxGroupPanel :visible="true"/>
+      <DxPaging :page-size="12"/>
+      <DxGrouping :auto-expand-all="false"/>
+      <DxColumn
+        data-field="CustomerID"
+        caption="Customer"
+      >
+        <DxLookup
+          :data-source="customersData"
+          value-expr="Value"
+          display-expr="Text"
+        />
+      </DxColumn>
+      <DxColumn
+        data-field="OrderDate"
+        data-type="date"
+      />
+      <DxColumn data-field="Freight"/>
+      <DxColumn
+        data-field="ShipCountry"
+        :group-index="0"
+      />
+      <DxColumn
+        data-field="ShipVia"
+        caption="Shipping Company"
+        data-type="number"
+        :group-index="1"
+      >
+        <DxLookup
+          :data-source="shippersData"
+          value-expr="Value"
+          display-expr="Text"
+        />
+      </DxColumn>
+      <DxColumn
+        data-field="ShipCity"
+        :group-index="2"
+      />
+    </DxDataGrid>
   </div>
 </template>
