2fdcfa26b72768054cfacc984c6475fa9260b309
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2025-12-13 10:54:28 +0200

    Migrate to eslint-config-devextreme 1.1.8

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,9 +1,5 @@
-import { Component, ViewChild } from '@angular/core';
-import * as AspNetData from 'devextreme-aspnet-data-nojquery';
-import { DxDataGridComponent } from 'devextreme-angular/ui/data-grid';
-import type { IGroupRowReadyParameter } from './GroupRowSelection/group-row-component/group-row.component';
-import GroupSelectionHelper from './GroupRowSelection/GroupRowSelectionHelper';
-import { localData } from './localdata';
+import { Component } from '@angular/core';
+import { ClickEvent } from 'devextreme/ui/button';
 
 @Component({
   selector: 'app-root',
@@ -11,53 +7,14 @@ import { localData } from './localdata';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  dataSource: AspNetData.CustomStore;
+  title = 'Angular';
 
-  customersData: AspNetData.CustomStore;
+  counter = 0;
 
-  shippersData: AspNetData.CustomStore;
+  buttonText = 'Click count: 0';
 
-  @ViewChild(DxDataGridComponent) grid!: DxDataGridComponent;
-
-  selectedRowKeys: any[] = [10521];
-
-  selectionFilter: any[] = ['OrderID', '=', 10521];
-
-  helper: GroupSelectionHelper | undefined;
-
-  localData: any[];
-
-  constructor() {
-    const url = 'https://js.devexpress.com/Demos/Mvc/api/DataGridWebApi';
-    this.localData = localData;
-    this.dataSource = AspNetData.createStore({
-      key: 'OrderID',
-      loadUrl: `${url}/Orders`,
-      onBeforeSend(_method: string, ajaxOptions: { xhrFields?: { withCredentials?: boolean } }) {
-        ajaxOptions.xhrFields = { withCredentials: true };
-      },
-    });
-    this.customersData = AspNetData.createStore({
-      key: 'Value',
-      loadUrl: `${url}/CustomersLookup`,
-      onBeforeSend(_method: string, ajaxOptions: { xhrFields?: { withCredentials?: boolean } }) {
-        ajaxOptions.xhrFields = { withCredentials: true };
-      },
-    });
-    this.shippersData = AspNetData.createStore({
-      key: 'Value',
-      loadUrl: `${url}/ShippersLookup`,
-      onBeforeSend(_method: string, ajaxOptions: { xhrFields?: { withCredentials?: boolean } }) {
-        ajaxOptions.xhrFields = { withCredentials: true };
-      },
-    });
-  }
-
-  ngAfterViewInit(): void {
-    this.helper = new GroupSelectionHelper(this.grid.instance);
-  }
-
-  groupRowInit(arg: IGroupRowReadyParameter): void {
-    this.helper?.groupRowInit(arg);
+  onClick(_e: ClickEvent): void {
+    this.counter++;
+    this.buttonText = `Click count: ${this.counter}`;
   }
 }
