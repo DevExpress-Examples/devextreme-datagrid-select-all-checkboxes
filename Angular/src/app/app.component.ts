87f6100a22edbc73ababf0024b8490f186376fed
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-04-01 09:31:50 +0300

    Restore Angular app.component.ts

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,21 +1,64 @@
-import { Component } from '@angular/core';
-import { ClickEvent } from 'devextreme/ui/button';
+import { Component, ViewChild } from '@angular/core';
+import * as AspNetData from 'devextreme-aspnet-data-nojquery';
+import { DxDataGridComponent } from 'devextreme-angular/ui/data-grid';
+import type { IGroupRowReadyParameter } from './GroupRowSelection/group-row-component/group-row.component';
+import GroupSelectionHelper from './GroupRowSelection/GroupRowSelectionHelper';
+import { localData } from './localdata';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
-    standalone: false
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  standalone: false,
 })
 export class AppComponent {
-  title = 'Angular';
+  dataSource: AspNetData.CustomStore;
 
-  counter = 0;
+  customersData: AspNetData.CustomStore;
 
-  buttonText = 'Click count: 0';
+  shippersData: AspNetData.CustomStore;
 
-  onClick(_e: ClickEvent): void {
-    this.counter++;
-    this.buttonText = `Click count: ${this.counter}`;
+  @ViewChild(DxDataGridComponent) grid!: DxDataGridComponent;
+
+  selectedRowKeys: any[] = [10521];
+
+  selectionFilter: any[] = ['OrderID', '=', 10521];
+
+  helper: GroupSelectionHelper | undefined;
+
+  localData: any[];
+
+  constructor() {
+    const url = 'https://js.devexpress.com/Demos/Mvc/api/DataGridWebApi';
+    this.localData = localData;
+    this.dataSource = AspNetData.createStore({
+      key: 'OrderID',
+      loadUrl: `${url}/Orders`,
+      onBeforeSend(_method: string, ajaxOptions: { xhrFields?: { withCredentials?: boolean } }) {
+        ajaxOptions.xhrFields = { withCredentials: true };
+      },
+    });
+    this.customersData = AspNetData.createStore({
+      key: 'Value',
+      loadUrl: `${url}/CustomersLookup`,
+      onBeforeSend(_method: string, ajaxOptions: { xhrFields?: { withCredentials?: boolean } }) {
+        ajaxOptions.xhrFields = { withCredentials: true };
+      },
+    });
+    this.shippersData = AspNetData.createStore({
+      key: 'Value',
+      loadUrl: `${url}/ShippersLookup`,
+      onBeforeSend(_method: string, ajaxOptions: { xhrFields?: { withCredentials?: boolean } }) {
+        ajaxOptions.xhrFields = { withCredentials: true };
+      },
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.helper = new GroupSelectionHelper(this.grid.instance);
+  }
+
+  groupRowInit(arg: IGroupRowReadyParameter): void {
+    this.helper?.groupRowInit(arg);
   }
 }
