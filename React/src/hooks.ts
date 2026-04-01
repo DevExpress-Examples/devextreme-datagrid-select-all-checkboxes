db98d4b654899b47e1f9a546e0d5042720a06604
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-04-01 09:32:46 +0300

    Remove no-unused-vars

--- React/src/hooks.ts
+++ React/src/hooks.ts
@@ -1,9 +1,7 @@
 import { useRef, useLayoutEffect, useCallback } from 'react';
 
 export function useEventCallback<Args extends unknown[], Return>(
-  // eslint-disable-next-line no-unused-vars
   fn: (...args: Args) => Return,
-  // eslint-disable-next-line no-unused-vars
 ): (...args: Args) => Return {
   // eslint-disable-next-line @typescript-eslint/no-extra-parens, no-unused-vars
   const ref = useRef<((...args: Args) => Return) | null>(null);
--- Vue/src/types.ts
+++ Vue/src/types.ts
@@ -1,5 +1,4 @@
 export interface IGroupRowReadyParameter {
   key: string[];
-  // eslint-disable-next-line no-unused-vars
   setCheckedState: (state: boolean | undefined) => void;
 }
